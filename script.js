02aa2aa867ab6a3c8920df9ac6eb6b15df107ab9
JuanPedro-Soares <juanpedrosoares1@gmail.com>
2023-11-03 15:46:38 -0300

    feat: adding all files

---
+++ script.js
@@ -0,0 +1,53 @@
+function ativar(){
+const textos = document.querySelectorAll('.textos');
+let currentIndex = 0;
+
+function alternarAtivo() {
+    textos[currentIndex].classList.remove('ativo');
+    currentIndex = (currentIndex + 1);
+    if(currentIndex===3){
+        currentIndex=0;
+    }
+    textos[currentIndex].classList.add('ativo');
+}
+
+setInterval(alternarAtivo, 4000);
+}
+const controls = document.querySelectorAll('.control');
+
+let currentIndex = 1;
+const items = document.querySelectorAll('.item');
+const maxitens = items.length;
+
+controls.forEach(control =>{
+    control.addEventListener('click',()=>{
+        const isLeft = control.classList.contains('arrow-left');
+         if(isLeft){
+            currentIndex-= 1
+         }
+         else{
+            currentIndex+=1
+         }
+         if(currentIndex>=maxitens){
+            currentIndex=0;        
+         }
+         if(currentIndex<0){
+            currentIndex= maxitens-1;
+         }
+
+        items.forEach(item => item.classList.remove('current-item'))
+
+        items[currentIndex].classList.add('current-item')
+
+        items[currentIndex].scrollIntoView({
+            inline:"center",
+            behavior: "smooth",
+            block: 'nearest',
+        })
+
+        
+    })
+})
+
+
+ativar();
